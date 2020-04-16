150b8060d50ed0c1b0088afed1d37ec9de9cd7c2
JimHiggins <jim242@hotmail.co.uk>
2020-04-16 10:42:51 +0100

    socket connection front and back

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var socket = require('socket.io');
 
 //App Setup
 var app = express();
@@ -7,4 +8,12 @@ var server = app.listen(4000, function(){
     console.log('Running on port 4000')
 });
 
+//Static files
 app.use(express.static('public'));
+
+//Socket setup --server backend side
+var io = socket(server);
+
+io.on('connection', function(socket){
+    console.log('made socket connection', socket.id)
+});
---
+++ public/chat.js
@@ -0,0 +1,2 @@
+//Make connection -- front end side
+var socket = io.connect('http://localhost:4000');
