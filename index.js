3f734078d997dc0a29f86da59a456df93c9553a8
JimHiggins <jim242@hotmail.co.uk>
2020-04-16 11:16:23 +0100

    chat broadcast added

--- index.js
+++ index.js
@@ -15,5 +15,14 @@ app.use(express.static('public'));
 var io = socket(server);
 
 io.on('connection', function(socket){
-    console.log('made socket connection', socket.id)
+    console.log('made socket connection', socket.id);
+
+    socket.on('chat', function(data){
+        io.sockets.emit('chat', data);
+    });
+
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    })
 });
+
